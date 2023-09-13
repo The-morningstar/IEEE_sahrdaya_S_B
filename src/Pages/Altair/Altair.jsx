b692a504cfb47eee0b7add61ccb558627798cdc9
Vivek K J <vivekkj2004@gmail.com>
2023-09-13 11:36:30 +0530

    When altair page is loaded, scroll to top, instead of current scroll position

--- src/Pages/Altair/Altair.jsx
+++ src/Pages/Altair/Altair.jsx
@@ -1,34 +1,33 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-
-import Footer from '../../components/Footer';
-import Nabar  from '../../Altair_components/Navbar/Navbar';
-import AltHero from '../../Altair_components/Hero/Hero';
-import AltGallery from '../../Altair_components/Gallary/gallary';
-import AltAboutSection from '../../Altair_components/Earlybird/AltAbout';
-import CountdownTimer from '../../Altair_components/Timer/Timer';
-import Altreg from '../../Altair_components/Registration/Altreg';
-
+import Footer from "../../components/Footer";
+import Nabar from "../../Altair_components/Navbar/Navbar";
+import AltHero from "../../Altair_components/Hero/Hero";
+import AltGallery from "../../Altair_components/Gallary/gallary";
+import AltAboutSection from "../../Altair_components/Earlybird/AltAbout";
+import CountdownTimer from "../../Altair_components/Timer/Timer";
+import Altreg from "../../Altair_components/Registration/Altreg";
 
 const Altair = () => {
+  // scroll to top when navigated from other pages
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
   return (
-   
-    <section >
-      
-       <Nabar/>
-       
-       <section id ='althome'><AltHero/></section>
-       <CountdownTimer/>
-       <AltAboutSection/>
-       <Altreg/>
-      
-       
-       <AltGallery/>
-       
-       <Footer/>
-     
+    <section>
+      <Nabar />
+
+      <section id="althome">
+        <AltHero />
+      </section>
+      <CountdownTimer />
+      <AltAboutSection />
+      <Altreg />
+
+      <AltGallery />
+
+      <Footer />
     </section>
-    
   );
 };
 
